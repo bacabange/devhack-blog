8d0e72a15a7ee4ff62a1518bb1d9db9cd3024419
Stiven <stivencastillo.90@gmail.com>
2018-09-14 00:26:08 -0500

    Usar highcharts dentro de react /post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './post.css';
 import { getPost } from '../utils/api';
+import Highcharts from 'highcharts';
 
 // Consumir API para traer el detalle del post https://jsonplaceholder.typicode.com/posts/:id
 export default class Post extends Component {
@@ -14,6 +15,33 @@ export default class Post extends Component {
   }
 
   componentDidMount() {
+    Highcharts.chart('chart-container', {
+      title: {
+        text: 'Logarithmic axis demo',
+      },
+
+      xAxis: {
+        tickInterval: 1,
+      },
+
+      yAxis: {
+        type: 'logarithmic',
+        minorTickInterval: 0.1,
+      },
+
+      tooltip: {
+        headerFormat: '<b>{series.name}</b><br />',
+        pointFormat: 'x = {point.x}, y = {point.y}',
+      },
+
+      series: [
+        {
+          data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+          pointStart: 1,
+        },
+      ],
+    });
+
     const id = this.props.match.params.id;
     getPost(id)
       .then(response => {
@@ -30,6 +58,8 @@ export default class Post extends Component {
       <div className="post-detail">
         <h1>{this.state.title}</h1>
         <p>{this.state.body}</p>
+
+        <div id="chart-container"> </div>
       </div>
     );
   }
