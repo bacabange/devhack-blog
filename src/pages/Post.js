8b19f4454c14fafa0c4c79cd153dea515a1f1bba
Stiven <stivencastillo.90@gmail.com>
2018-09-14 20:16:55 -0500

    Traer el single post mediante redux y actions

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import './post.css';
 import { getPost } from '../utils/api';
+import { connect } from 'react-redux';
+import * as singlePostActions from '../actions/singlePostActions';
 
 // Consumir API para traer el detalle del post https://jsonplaceholder.typicode.com/posts/:id
-export default class Post extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      title: '',
-      body: '',
-    };
-  }
-
+class Post extends Component {
   componentDidMount() {
+    console.log(this.props);
     const id = this.props.match.params.id;
-    getPost(id)
-      .then(response => {
-        this.setState({
-          title: response.data.title,
-          body: response.data.body,
-        });
-      })
-      .catch(error => console.log(error));
+
+    this.props.getFetch(`/posts/${id}`, 'get');
   }
 
   render() {
+    const { title, body } = this.props.post;
     return (
       <div className="post-detail">
-        <h1>{this.state.title}</h1>
-        <p>{this.state.body}</p>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
 }
+
+// Fuera de clase
+function mapStateToProps(state) {
+  return {
+    post: state.singlePostReducer.post,
+    loading: state.singlePostReducer.loading,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  singlePostActions
+)(Post);
